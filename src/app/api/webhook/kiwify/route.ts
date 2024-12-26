4db97ad4bf719c4f9c7ad07b28331f26d8dcf640
Wesley Leandro <wesleyleandro@penzack.com>
2024-12-26 13:12:10 -0300

    feat: fix webhook

--- src/app/api/webhook/kiwify/route.ts
+++ src/app/api/webhook/kiwify/route.ts
@@ -1,7 +1,8 @@
 export async function POST(request: Request) {
-  const { event } = await request.json()
+  //   const { event } = await request.json()
+  const data = request.body
 
-  console.log('Received event:', event)
+  console.log('Received data:', data)
 
   return Response.json({ received: true }, { status: 200 })
 }
