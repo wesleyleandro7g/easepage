1a45d20612eebac152bd0980e8aec1ec7fcd451f
Wesley Leandro <wesleyleandro@penzack.com>
2025-03-06 20:10:25 -0300

    feat: adds a fallback

--- src/app/api/webhook/kiwify/route.ts
+++ src/app/api/webhook/kiwify/route.ts
@@ -6,37 +6,46 @@ import { WebhookEventType } from './type'
 export async function POST(request: Request) {
   const data = (await request.json()) as WebhookEventType
 
-  //   const twilioClient = twilio(
-  //     process.env.TWILIO_ACCOUNT_SID,
-  //     process.env.TWILIO_AUTH_TOKEN
-  //   )
+  try {
+    //   const twilioClient = twilio(
+    //     process.env.TWILIO_ACCOUNT_SID,
+    //     process.env.TWILIO_AUTH_TOKEN
+    //   )
 
-  console.log('Webhook received:', data)
+    console.log('Webhook received:', data)
 
-  const clientServer = createServer()
+    const clientServer = createServer()
 
-  await clientServer
-    .from('pages')
-    .update({
-      is_active: true,
-    })
-    .eq('id', data.TrackingParameters.src)
+    if (data.webhook_event_type === 'order_approved') {
+      if (data.TrackingParameters.src) {
+        await clientServer
+          .from('pages')
+          .update({
+            is_active: true,
+          })
+          .eq('id', data.TrackingParameters.src)
+      } else {
+        console.log('No data.TrackingParameters.src received')
+      }
+      //   if (data.webhook_event_type === 'order_approved') {
+      //     const costumerName = data.Customer.first_name
+      //     const phoneNumber = data.Customer.mobile
 
-  //   if (data.webhook_event_type === 'order_approved') {
-  //     const costumerName = data.Customer.first_name
-  //     const phoneNumber = data.Customer.mobile
+      //     const message = `Olá ${costumerName}, sua compra foi aprovada com sucesso! Obrigado por confiar em nós.`
 
-  //     const message = `Olá ${costumerName}, sua compra foi aprovada com sucesso! Obrigado por confiar em nós.`
+      //     await twilioClient.messages.create({
+      //       from: 'whatsapp:+14155238886', // Número WhatsApp do Twilio
+      //       to: `whatsapp:${phoneNumber}`, // Número do cliente
+      //       body: message,
+      //     })
 
-  //     await twilioClient.messages.create({
-  //       from: 'whatsapp:+14155238886', // Número WhatsApp do Twilio
-  //       to: `whatsapp:${phoneNumber}`, // Número do cliente
-  //       body: message,
-  //     })
+      //     console.log('Mensagem enviada com sucesso!')
+      //     console.log(`Order approved for ${name} with phone ${phoneNumber}`)
+      //   }
+    }
 
-  //     console.log('Mensagem enviada com sucesso!')
-  //     console.log(`Order approved for ${name} with phone ${phoneNumber}`)
-  //   }
-
-  return Response.json({ received: true }, { status: 200 })
+    return Response.json({ received: true }, { status: 200 })
+  } catch (error) {
+    return Response.json(error, { status: 400 })
+  }
 }
