8612f844ad7fcb2ccb8c32ee6ebe92ae331c19d7
Wesley Leandro <wesleyleandro@penzack.com>
2025-03-09 20:48:13 -0300

    feat: implements update page status when webhook received

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,27 +1,23 @@
-// import { createServer } from '@/db/supabase/server'
-
-import { WebhookEventType } from './type'
+import { createServer } from '@/db/supabase/server'
 
 export async function POST(request: Request) {
-  const data = (await request.json()) as WebhookEventType
+  const data = await request.json()
 
-  console.log('Webhook received:', data)
   try {
-    // const clientServer = createServer()
+    const clientServer = createServer()
 
-    // if (data.webhook_event_type === 'order_approved') {
-    //   if (data.TrackingParameters.src) {
-    //     await clientServer
-    //       .from('pages')
-    //       .update({
-    //         is_active: true,
-    //       })
-    //       .eq('id', data.TrackingParameters.src)
-    //   } else {
-    //     console.log('No data.TrackingParameters.src received')
-    //   }
+    if (data.type === 'checkout.session.completed') {
+      const pageId = data.data.object.client_reference_id
 
-    // }
+      await clientServer
+        .from('pages')
+        .update({
+          is_active: true,
+        })
+        .eq('id', pageId)
+    } else {
+      console.log('PAGE ID NOT FOUND')
+    }
 
     return Response.json({ received: true }, { status: 200 })
   } catch (error) {
